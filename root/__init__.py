3874702c1620c6114d6050e1a0512679f49a7c3a
Constantinos Psomadakis <tinosps@gmail.com>
2022-07-24 22:25:29 +0100

    Fixed silly fpath bug

--- root/__init__.py
+++ root/__init__.py
@@ -2,9 +2,9 @@ from flask import Flask
 import json
 
 app = Flask(__name__)
-with open("__private__/config.json", "r") as f:
+
+with open("root/__private__/config.json", "r") as f:
     data = json.load(f)
     app.config['SECRET_KEY'] = data["SECRET_KEY"]
-    
 
 from root import routes
