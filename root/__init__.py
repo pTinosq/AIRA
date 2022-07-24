fb8b4fae2123489bf1b9b2c3bd0fdbca9b19af13
Constantinos Psomadakis <tinosps@gmail.com>
2022-07-24 22:21:18 +0100

    lol I commited the secret key

--- root/__init__.py
+++ root/__init__.py
@@ -1,8 +1,10 @@
 from flask import Flask
+import json
 
 app = Flask(__name__)
-
-
-app.config['SECRET_KEY'] = '22f32f52f73aeaf08ece955bdc6642e64f8004663bfeffa8'
+with open("__private__/config.json", "r") as f:
+    data = json.load(f)
+    app.config['SECRET_KEY'] = data["SECRET_KEY"]
+    
 
 from root import routes
